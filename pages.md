8ad3aa2cf9daabb6b57daa060f6737260f9c8786
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-08-17 22:29:38 +0100

    Add pages and mkpages.py
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

---
+++ pages.md
@@ -0,0 +1,5 @@
+# Pages
+- [linux](linux/)
+- [linux/kernel](linux/kernel)
+	- [linux/arch_linux/razer_blade_15](linux/arch_linux/razer_blade_15)
+	- [linux/arch_linux](linux/arch_linux/)
---
+++ tools/mkpages.py
@@ -0,0 +1,27 @@
+root_folders = ['linux']
+
+files = []
+titles = []
+
+import os
+
+for root_folder in root_folders:
+	for root, dirs, file in os.walk(root_folder):
+		for f in file:
+			if f.endswith('.md'):
+				if f == 'README.md':
+					files.append(os.path.join(root, f).replace('README.md', ''))
+					titles.append(os.path.join(root, f).replace('/README.md', ''))
+				else:
+					files.append(os.path.join(root, f).replace('.md', ''))
+					titles.append(os.path.join(root, f).replace('.md', ''))
+
+with open('pages.md', 'w') as f:
+	f.write('# Pages\n')
+	for i in range(len(files)):
+		indents = files[i].count('/') - 1
+
+		for j in range(indents):
+			f.write('\t')
+		
+		f.write('- [' + titles[i] + '](' + files[i] + ')\n')
